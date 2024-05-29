421e06fb8e93f42de00db6c78a8b7b7fc2c08350
theo.oberli <theo.oberli@divcom.ch>
2024-05-29 09:45:15 +0200

    Menu, composants et vue faites

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AccueilView from '../views/AccueilView.vue'
+import ServiceClientView from '../views/ServiceClientView.vue'
+import FicheSabreView from '../views/FicheSabreView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +9,21 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: AccueilView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/service-client',
+      name: 'service-client',
+      component: ServiceClientView
+    },
+    {
+      path: '/sabre/:id',
+      name: 'sabre',
+      component: FicheSabreView,
+      props: true
     }
   ]
 })
 
 export default router
+
---
+++ src/views/AccueilView.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="page-accueil">
+    <h1>Découvrez nos sabres <strong>LASER</strong></h1>
+
+    <div class="sabres">
+      <div class="sabre">
+        <router-link to="/sabre/1">
+          <figure>
+            <img src="/img/sl-cal.jpg" alt="Sabre laser Cal Kestis">
+            <figcaption>
+              <h2>Sabre laser Cal Kestis</h2>
+              <div class="prix">349 CHF</div>
+            </figcaption>
+          </figure>
+        </router-link>
+      </div>
+      <div class="sabre rupture">
+        <router-link to="/sabre/1">
+          <figure>
+            <img src="/img/sl-cal.jpg" alt="Sabre laser Cal Kestis">
+            <figcaption>
+              <p>Noooon, les stocks sont vides !</p>
+              <h2>Sabre laser Cal Kestis</h2>
+              <div class="prix">349 CHF</div>
+            </figcaption>
+          </figure>
+        </router-link>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
